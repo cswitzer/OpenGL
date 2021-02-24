20cba2431623f4ec1c42776fe6ca5cbe83231762
cswitzer <50593813+cswitzer@users.noreply.github.com>
2021-02-23 17:20:02 -0700

    Created full screen mode and counted fps

--- OpenGL/OpenGL/src/lesson1.cpp
+++ OpenGL/OpenGL/src/lesson1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <sstream>
 #define GLEW_STATIC
 #include "GL/glew.h";
 #include "GLFW/glfw3.h";
@@ -9,8 +9,14 @@
 const char* APP_TITLE = "Basic Game";
 const float gWindowWidth = 800;
 const float gWindowHeight = 600;
+bool gFullScreen = false;
 
 using namespace std;
+
+// key pressing function must be like this exactly (the function can be named anything though)
+// This function will be called everytime a key is hit
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
 
 int main()
 {
@@ -27,7 +33,24 @@ int main()
 	// We want to force the application to use only the newest version of OpenGL
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	GLFWwindow* pWindow = NULL;
+
+	if (gFullScreen) {
+		// reference the monitor we are using right now
+		GLFWmonitor* pMonitor = glfwGetPrimaryMonitor();
+
+		// get the specs of our monitor
+		const GLFWvidmode* pVmode = glfwGetVideoMode(pMonitor);
+
+		// if a monitor is found, use that monitor
+		if (pVmode != NULL) {
+			pWindow = glfwCreateWindow(pVmode->width, pVmode->height, APP_TITLE, pMonitor, NULL);
+		}
+	}
+	else {
+		pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	}
+
 	// if the wind fails to be created, exit immediately
 	if (pWindow == NULL)
 	{
@@ -39,6 +62,9 @@ int main()
 
 	// make our current window the window we draw to! 
 	glfwMakeContextCurrent(pWindow);
+
+	// callback functions that handle user input
+	glfwSetKeyCallback(pWindow, glfw_onKey);
 
 	// glew queries our video card and finds all the function pointers to all the functions we need
 	glewExperimental = GL_TRUE; // mandatory for new versions of modern OpenGL
@@ -52,6 +78,8 @@ int main()
 	// stay in this loop until the window should be closed
 	while (!glfwWindowShouldClose(pWindow))
 	{
+		showFPS(pWindow);
+
 		// poll for keyboard and mouse inputs
 		glfwPollEvents();
 
@@ -67,3 +95,41 @@ int main()
 	return 0;
 }
 
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// bailing out of the while loop in main if the escape key is pressed
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+void showFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started as a double
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// limit text update 4 times per second
+	if (elapsedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		// the frame count per second
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		// output frames per second to the screen
+		std::ostringstream outs;
+		outs.precision(3);
+		// append to after our title
+		outs << std::fixed << APP_TITLE << "   "
+			<< "FPS: " << fps << "   " << "Frame Time: "
+			<< msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		frameCount = 0;
+	}
+	frameCount++;
+}
